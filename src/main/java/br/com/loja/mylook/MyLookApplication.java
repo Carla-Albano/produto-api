979d88731ce374ff939876ae1d9a05967a2d1465
Carla-Albano <carla_k3albano@hotmail.com>
2021-04-01 20:04:19 -0300

    primeiro commit

---
+++ src/main/java/br/com/loja/mylook/MyLookApplication.java
@@ -0,0 +1,13 @@
+package br.com.loja.mylook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyLookApplication.class, args);
+	}
+
+}
